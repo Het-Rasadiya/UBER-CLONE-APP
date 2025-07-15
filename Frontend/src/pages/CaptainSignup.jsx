391e70613a22eeb4e957416720253253bf29c379
Het Rasadiya <het.rasadiya@gmail.com>
2025-07-15 21:03:50 +0530

    Captain Sifnup Change

--- Frontend/src/pages/CaptainSignup.jsx
+++ Frontend/src/pages/CaptainSignup.jsx
@@ -169,7 +169,7 @@ const CaptainSignup = () => {
         </form>
       </div>
       <div>
-        <p className="text-[10px] leading-tight">
+        <p className="text-[10px] mt-6 leading-tight">
           This site is protected by reCAPTCHA and the{" "}
           <span className="underline">Google Privacy Policy </span>and{" "}
           <span className="underline">Terms of Services apply</span>.
